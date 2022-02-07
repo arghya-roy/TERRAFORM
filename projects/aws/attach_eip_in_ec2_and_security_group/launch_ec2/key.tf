43c73a46f58e574f07992dd4b56f58953fbc3846
arghya-roy <95282173+arghya-roy@users.noreply.github.com>
2022-02-08 04:04:44 +0530

    Update key.tf

--- projects/aws/attach_eip_in_ec2_and_security_group/launch_ec2/key.tf
+++ projects/aws/attach_eip_in_ec2_and_security_group/launch_ec2/key.tf
@@ -1,3 +1,6 @@
+#ssh-keygen -f mykeypair
+# then copy both public and private key and paste them in launch_ec2 folder.
+
 resource "aws_key_pair" "mykeypair" {
   key_name   = "mykeypair"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
