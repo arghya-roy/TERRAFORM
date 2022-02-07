c627511874fce2842ab4affa707061bf688253fc
arghya-roy <95282173+arghya-roy@users.noreply.github.com>
2022-02-08 02:33:47 +0530

    Update vpc.tf

--- projects/aws/attach_eip_in_ec2_and_security_group/VPC/public&private&igw&nat/vpc.tf
+++ projects/aws/attach_eip_in_ec2_and_security_group/VPC/public&private&igw&nat/vpc.tf
@@ -91,7 +91,7 @@ resource "aws_route_table" "main-public" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main-gw.id
+    gateway_id = aws_internet_gateway.main-gw.id  # it will create local target by default
   }
 
   tags = {
