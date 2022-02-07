cb315bdb92c325ab34e509f5f9dd7f113cd462f1
arghya-roy <95282173+arghya-roy@users.noreply.github.com>
2022-02-08 02:43:11 +0530

    Update vpc.tf

--- projects/aws/attach_eip_in_ec2_and_security_group/VPC/public&private&igw&nat/vpc.tf
+++ projects/aws/attach_eip_in_ec2_and_security_group/VPC/public&private&igw&nat/vpc.tf
@@ -1,8 +1,8 @@
 # Internet VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
-  instance_tenancy     = "default"
-  enable_dns_support   = "true"
+  instance_tenancy     = "default"   # it means multiple ec2 can connects with this vpc.
+  enable_dns_support   = "true"   
   enable_dns_hostnames = "true"
   enable_classiclink   = "false"
   tags = {
